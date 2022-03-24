f55a8804aeae22b186b601b76df13943b2a39b74
jay183145 <jay183145@gmail.com>
2022-03-24 16:21:19 +0800

    change loader from remove css to fadeinout

--- login-site/scripts/main.js
+++ login-site/scripts/main.js
@@ -1,9 +1,11 @@
+//DOMContentloaded -> interactive
+//to prevent any jQuery code from running before the document is finished loading
 $(document).ready(function(){
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse');
 
     /* click event on toggle menu */
-    $toggleCollapse.click(function(){
+    $toggleCollapse.on("click", function () {
         $nav.toggleClass("collapse");
     })
 });
